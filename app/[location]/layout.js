d78935f45ab8df9a9f618279353e3b643e93fa63
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-24 02:47:43 +0600

    complete home page part 30%

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 // import "../globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,16 +9,38 @@ export const metadata = {
   description: "World tempreture app",
 };
 
-export default function RootLayout({ children,aqi,tempreture,weather,wind }) { //same folder e deikha aqi k ai layout e access dilam
+export default function RootLayout({
+  children,
+  aqi,
+  tempreture,
+  weather,
+  wind,
+}) {
+  //same folder e deikha aqi k ai layout e access dilam
   return (
     <div className="wrapper">
-        {children}
-        {weather}
-        {aqi}
-        {wind}
-        {tempreture}
+      <div>
+        <div className="overlay" />
+        <Image
+          src="/background.png"
+          alt="background imgage"
+          width={800}
+          height={1200}
+          className="bg-img"
+        />
+      </div>
 
+      <main className="!z-50 w-full">
+        <div className="container">
+          <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+            {children}
+            {weather}
+            {aqi}
+            {wind}
+            {tempreture}
+          </div>
+        </div>
+      </main>
     </div>
-      
   );
 }
