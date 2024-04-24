802c6ade489987dfdb427c9b70adf8e4e90a9071
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-24 16:10:22 +0600

    loading is complete and home page always redirect longitude and latitude pages

--- components/LocationDetector.jsx
+++ components/LocationDetector.jsx
@@ -1,42 +1,52 @@
-'use client'
+"use client";
 
-import {useState,useEffect} from 'react';
-import { useSearchParams,usePathname,useRouter } from 'next/navigation';
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
 
 const LocationDetector = () => {
+  //firts we handle loading
+  const [loading, setLoading] = useState(false);
 
-    //firts we handle loading
-    const [loading,setLoading] = useState(false)
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const router = useRouter();
 
-    const searchParams = useSearchParams()
-    const pathName = usePathname()
-    const router = useRouter()
+  useEffect(() => {
+    setLoading(true);
 
-    useEffect(()=>{
-        setLoading(true)
-        
-        //get url parameter
-        const params = URLSearchParams(searchParams)
+    //get url parameter
+    const params = new URLSearchParams(searchParams);
 
-        // now we need geolocation , in browser property 'navigator'
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition((position) =>{  // ai position er modde latitude and longitude available thake
-                params.set('latitude',position.coords.latitude)
-                params.set('longitude',position.coords.longitude)
-                setLoading(false)
-                //redirect korbo /current?latitude=234&lognitude=343
-                router.push(`/current?${params.toString( )}`)
-            })
-        }
+    // now we need geolocation , in browser property 'navigator'
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // ai position er modde latitude and longitude available thake
+        params.set("latitude", position.coords.latitude);
+        params.set("longitude", position.coords.longitude);
+        setLoading(false);
+        //redirect korbo /current?latitude=234&lognitude=343
+        router.push(`/current?${params.toString()}`);
+      });
+    }
+  }, [pathName, searchParams]);
 
-    },[])
-
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
+      {loading && (
+        <>
+          <Image
+          src='/giphy.gif'
+            width={500}
+            height={700}
+            alt="Loading animation"
+            className=" border rounded-md my-4"
+          />
+          <p>Detecting Location...</p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default LocationDetector;
