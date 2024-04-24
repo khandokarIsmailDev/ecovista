3d9bdf976a455ec17a7fb297449dd84c37bbe41c
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-24 15:50:10 +0600

    get curent location from browser and set in params (modify params)

--- components/LocationDetector.jsx
+++ components/LocationDetector.jsx
@@ -19,6 +19,15 @@ const LocationDetector = () => {
         //get url parameter
         const params = URLSearchParams(searchParams)
 
+        // now we need geolocation , in browser property 'navigator'
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((position) =>{  // ai position er modde latitude and longitude available thake
+                params.set('latitude',position.coords.latitude)
+                params.set('longitude',position.coords.longitude)
+                setLoading(false)
+            })
+        }
+
     },[])
 
     return (
